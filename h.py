35f40ef155e70235348e9b5f6611aaf6b6ac5e64
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-11-06 14:54:20 +0100

    Update h.py

--- h.py
+++ h.py
@@ -1,11 +1,26 @@
-for side in range(3):
-	if side % 2 == 0:
-		hend.left(90)
-		hend.forward(100)
-		hend.right(180)
-		hend.forward(200)
-		hend.left(180)
-		hend.forward(100)
-	else:
-		hend.right(90)
-		hend.forward(100)
+import turtle
+hend = turtle.Turtle()
+hend.color('pink')
+
+
+
+def drawh():
+	for side in range(3):
+		if side % 2 == 0:
+			hend.left(90)
+			hend.forward(100)
+			hend.right(180)
+			hend.forward(200)
+			hend.left(180)
+			hend.forward(100)
+		else:
+			hend.right(90)
+			hend.forward(100)
+	app = str(input('do you wanna draw h  Y/N'))
+	if app == 'y' or app == 'Y':
+		drawh()
+
+drawh()
+
+
+	
